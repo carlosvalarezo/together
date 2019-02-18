94e55060481e2605cec27e5b072506b338afd360
carlos-valarezo-loaiza <carlos-valarezo-loaiza@carlos-valarezo-loaiza.local>
2019-02-18 17:29:34 -0500

    [CV] starts adding kotlin support

---
+++ togetherApp/ios/TogetherCameraView.swift
@@ -0,0 +1,9 @@
+//
+//  TogetherCameraView.swift
+//  togetherApp
+//
+//  Created by carlos-valarezo-loaiza on 2/18/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
