f41ac5d8d7518d903312789c6c134d4cfa576260
carlos-valarezo-loaiza <carlos-valarezo-loaiza@manchester.local>
2019-02-21 12:48:42 -0500

    [CV] adds parameters to start.sh script

--- togetherApp/start.sh
+++ togetherApp/start.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-commands=("$@")
+params=("$@")
 
-for arg in "${commands[@]}"; do
+get_command_line_params () {
+ for arg in "${params[@]}"; do
    if [ "$arg" = "run_ios" ]; then
       react-native run-ios
+   elif [ "$arg" = "start_ios_project" ]; then
+           npm install
+           react-native link
+           react-native run-ios
    else
       echo "command not found"
    fi
-done
+ done
+}
+
+get_command_line_params
+
